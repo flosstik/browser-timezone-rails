cf117f284a2b2c0e5c43a3c956cc2517cbf38502
German Kornev <gerakornev@mail.ru>
2015-11-09 09:56:57 -0800

    fix require

--- app/assets/javascripts/browser_timezone_rails/application.js
+++ app/assets/javascripts/browser_timezone_rails/application.js
@@ -14,4 +14,26 @@
 //= require jquery_ujs
 //= require jquery.cookie
 //= require jstz
-//= require set_time_zone.js
+
+
+window.BrowserTZone || (window.BrowserTZone = {});
+
+function get_tz_name(){
+    if(typeof Intl !== "undefined" && Intl !== null){
+        return Intl.DateTimeFormat().resolvedOptions().timeZone;
+    }
+    else{
+        return jstz.determine().name();
+    }
+}
+
+BrowserTZone.setCookie = function() {
+    return $.cookie("browser.timezone", get_tz_name(), {
+        expires: 365,
+        path: '/'
+    });
+};
+
+jQuery(function() {
+    return BrowserTZone.setCookie();
+});
